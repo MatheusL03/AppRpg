184177b60210d67ffd8c4d7e5f0c988a35624803
MatheusL03 <mathuesl_3@hotmail.com>
2024-11-26 22:23:56 -0300

    Aula 17

--- AppRpgEtec/Services/Personagens/PersonagemService.cs
+++ AppRpgEtec/Services/Personagens/PersonagemService.cs
@@ -56,7 +56,16 @@ namespace AppRpgEtec.Services.Personagens
             return result;
         }
 
+        public async Task<ObservableCollection<Personagem>> GetByNomeAproximadoAsync(string busca)
+        {
+            string urlComplementar = $"/GetByNomeAproximado/{busca}";
 
+            ObservableCollection<Models.Personagem> listaPersonagens = await
+            _request.GetAsync<ObservableCollection<Models.Personagem>>(_apiUrlBase + urlComplementar, _token);
+
+            return listaPersonagens;
+
+        }
 
     }
 }
--- AppRpgEtec/Views/Disputas/ListagemView.xaml.cs
+++ AppRpgEtec/Views/Disputas/ListagemView.xaml.cs
@@ -1,9 +1,15 @@
 namespace AppRpgEtec.Views.Disputas;
+using AppRpgEtec.ViewModels.Disputas;
 
 public partial class ListagemView : ContentPage
 {
+	DisputaViewModel viewModel;
+
 	public ListagemView()
 	{
 		InitializeComponent();
+
+		viewModel = new DisputaViewModel();
+		BindingContext = viewModel;
 	}
 }
